3e24fbc6bef614f5e04a8754daf6c372cfd54d39
odelasowmya <143280007+odelasowmya@users.noreply.github.com>
2024-05-16 18:10:45 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<!-- Mirrored from radixtouch.in/templates/snkthemes/grexa/source/light/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 06 Feb 2021 10:26:42 GMT -->
+<head>
+  <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+  <title>Grexa - Admin Dashboard Template</title>
+  <!-- General CSS Files -->
+  <link rel="stylesheet" type="text/css" href="assets/css/app.min.css">
+  <!-- Template CSS -->
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/components.css">
+  <link rel="stylesheet" type="text/css" href="assets/bundles/bootstrap-social/bootstrap-social.css">
+  <link rel="stylesheet" type="text/css" href="assets/bundles/flag-icon-css/css/flag-icon.min.css">
+  <!-- Custom style CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel='shortcut icon' type='image/x-icon' href='assets/img/favicon.ico' />
+    <!-- General JS Scripts -->
+    <script src="assets/js/app.min.js"></script>
+  
+  <!-- Page Specific JS File -->
+  <!-- <script src="assets/js/page/index.js"></script> -->
+  <!-- Template JS File -->
+  <script src="assets/js/scripts.js"></script>
+</head>
+
+<body>
+  <div class="loader"></div>
+  <div id="app">
+    <div class="main-wrapper main-wrapper-1">
+      
+      <div class="navbar-bg"></div>
+      <nav class="navbar navbar-expand-lg main-navbar">
+        <div class="form-inline mr-auto">
+          <ul class="navbar-nav mr-3">
+            <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg collapse-btn"><i
+                  class="fas fa-bars"></i></a></li>
+            
+        </ul>
+      </nav>
+      <div class="main-sidebar sidebar-style-2">
+        <aside id="sidebar-wrapper">
+          <div class="sidebar-brand">
+            <a href="index.html">
+              <img alt="image" src="assets/img/logo.png" class="header-logo" />
+              <span class="logo-name">Grexa</span>
+            </a>
+          </div>
+          <ul class="sidebar-menu" style="font-size: 16px;">
+          
+            <li class="dropdown active">
+              <a href="#" class="nav-link"><i class="fas fa-home"></i><span>Dashboard</span></a> 
+            </li>
+         
+            <li class="dropdown ">
+              <a href="#" class="nav-link has-dropdown"><i class="fas fa-leaf"></i><span>Domains</span></a>
+              <ul class="dropdown-menu" style="font-size: 16px;">
+                <li class=""><a class="nav-link" href="add-new-domain.html">Add New Domain </a></li>
+                <li class=""><a class="nav-link" href="list-of-domains.html">List of Domains </a></li>
+               </ul>
+            </li>
+            <li class="dropdown">
+              <a href="#" class="nav-link has-dropdown"><i class="far fa-chart-bar"></i><span>Categorys</span></a>
+              <ul class="dropdown-menu" style="font-size: 16px;">
+                <li><a class="nav-link" href="add-new-category.html">Add Category</a></li>
+                <li><a class="nav-link" href="list-of-category.html">List of Category</a></li>
+              </ul>
+            </li>
+            <li class="dropdown">
+              <a href="#" class="nav-link has-dropdown"><i class="fas fa-anchor"></i><span>List of Jobs</span></a>
+              <ul class="dropdown-menu" style="font-size: 16px;">
+                <li><a class="nav-link" href="add-new-job.html">Add Job</a></li>
+                <li><a class="nav-link" href="list-of-jobs.html">List of Jobs</a></li>
+               </ul>
+            </li>
+        </aside>
+        
+      </div>
+      <!-- Main Content -->
+
+    </div>
+  </div>
+
